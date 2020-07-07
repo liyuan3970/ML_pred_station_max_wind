3026f60d0c56ccc307e428c77e053e8946f9ffce
liyuan3970 <857956255@qq.com>
2020-07-07 22:29:09 +0800

    finshed

--- progress/main.py
+++ progress/main.py
@@ -6,16 +6,32 @@ from datetime import datetime, date, timedelta
 
 
 
-runtime ='05:00'
+down_time ='05:00'
 yesterday = (date.today() + timedelta(days = -1)).strftime("%Y-%m-%d")    # 昨天日期
 year = yesterday[2:4]
 month =yesterday[5:7]
 day = yesterday[8:10]
 
-del_time = '06:50'
-line_num = 2
-Contents = "wwwwwwww"
 
+del_time = '06:50'
+file_path ='io.ncl'
+line_num = 6
+Contents = "date = "+'"'+str(year)+str(month)+str(day)+'"'
+
+
+run_time = '07:00'
+
+
+
+up_time = '15:50'
+ip = "192.168.8.135"
+port = 22
+name ="liyuan3970"
+password = "123456"
+local_path = '/home/liyuan3970/test_demo/time/'
+remote_path = '/home/liyuan3970/demo/'
+filename ='a.txt'
+remote_file_name='b.txt'
 
 def download():
     '''处理时间函数'''
@@ -26,7 +42,7 @@ def download():
 
 
 
-def Del_line(file_path,line_num,Contents): #file_path:文件名；line_num：行号；Contents：修改后的内容
+def Del_line(): #file_path:文件名；line_num：行号；Contents：修改后的内容
     with open(file_path,"r") as f:
         res = f.readlines() #res 为列表
     res[line_num-1]=(Contents+"\n")  #删除行，因为索引是从 0 开始的，所以需要  -1
@@ -37,9 +53,8 @@ def Del_line(file_path,line_num,Contents): #file_path:文件名；line_num：行
 
 
 
-
 def run_ncl():
-	os.system("ncl io.ncl")
+    os.system("ncl io.ncl")
     time.sleep(20)
     print('ncl finshed------------------------------------------------------------------------')
 
@@ -47,16 +62,24 @@ def run_ncl():
 
 
 def upload():
-    pass
-
-
+    transport = paramiko.Transport((ip, port))    # 获取Transport实例
+    transport.connect(username=name, password=password)    # 建立连接
+    # 创建sftp对象，SFTPClient是定义怎么传输文件、怎么交互文件
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    # 将本地 a.txt 上传至服务器 /www/test.py。文件上传并重命名为b.txt
+    sftp.put(local_path+filename, remote_path+remote_file_name)
+    # 关闭连接
+    transport.close()
 
 
 
 
 if __name__ == '__main__':
-    print("主程序")
+    print("主程序",Contents,type(Contents))
+
     schedule.every().day.at(down_time).do(download)
     schedule.every().day.at(del_time).do(Del_line)
+    schedule.every().day.at(run_time).do(run_ncl)
+    schedule.every().day.at(up_time).do(upload)    
     while True:
         schedule.run_pending()
