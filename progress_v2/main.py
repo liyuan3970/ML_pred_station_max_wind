c0f6727a81887d830def25a8766bc7b5d7dfcd70
liyuan3970 <857956255@qq.com>
2020-07-16 19:17:16 +0800

    version

--- progress_v2/main.py
+++ progress_v2/main.py
@@ -12,15 +12,12 @@ today =(date.today()).strftime("%Y-%m-%d")
 year = yesterday[2:4]
 month =yesterday[5:7]
 day = yesterday[8:10]
-
-
-del_time = '06:50'
 file_path ='io.ncl'
-line_num = 6
+line_num = 6 # 更改文件中第六行（时间）的
 Contents = "date = "+'"'+str(year)+str(month)+str(day)+'"'
 
 
-run_time = '07:00'
+
 
 
 
@@ -35,29 +32,23 @@ filename = str(year)+str(month)+str(day)+'.nc'
 remote_file_name= 'ZJGRID.331000'+str(year)+str(month)+str(day)+'20'+'Wind03'+'.nc'
 
 def download():
-    '''处理时间函数'''
+    '''下载必要的数据'''
     print("Job4:每天06:00自动下载前一天20时向后预报一天的M4 10米风数据")
     os.system('java -jar GDSJavaClient.jar 10.135.29.64 8080 samba ECMWF_HR/10_METRE_WIND_GUST_IN_THE_LAST_3_HOURS'+' '+year+month+day+'20.027'+' '+year+month+day+'20.048'+' '+'diamond')
     time.sleep(20)
-    print('job finshed------------------------------------------------------------------------')
-
-
-
-def Del_line(): #file_path:文件名；line_num：行号；Contents：修改后的内容
+    '''更改ncl文件的时间'''
     with open(file_path,"r") as f:
         res = f.readlines() #res 为列表
     res[line_num-1]=(Contents+"\n")  #删除行，因为索引是从 0 开始的，所以需要  -1
     with open(file_path,"w") as f:
         f.write("".join(res))  #将 res 转换为 字符串重写写入到文本
-    return
-#Del_line("test.ncl",2,"wwwwwwww") #将第二行修改为 wwwwwwww
-
-
-
-def run_ncl():
+        f.close()
+    time.sleep(20)
+    #return
+    '''运行ncl文件'''
     os.system("ncl io.ncl")
     time.sleep(20)
-    print('ncl finshed------------------------------------------------------------------------')
+    print('job finshed------------------------------------------------------------------------')
 
 
 
@@ -79,8 +70,6 @@ if __name__ == '__main__':
     print("主程序",Contents,type(Contents),today)
 
     schedule.every().day.at(down_time).do(download)
-    schedule.every().day.at(del_time).do(Del_line)
-    schedule.every().day.at(run_time).do(run_ncl)
     schedule.every().day.at(up_time).do(upload)    
     while True:
         schedule.run_pending()
